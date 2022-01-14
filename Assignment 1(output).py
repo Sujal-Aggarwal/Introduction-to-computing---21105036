b407061e65294f8b793db5efabc41dd2d06c17ab
SujalAggarwal <97459859+Sujal-Aggarwal@users.noreply.github.com>
2022-01-14 18:06:49 +0530

    Add files via upload

---
+++ Assignment 1(output).py
@@ -0,0 +1,65 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\sujal\Desktop\Assignment 1.py ===============
+             Program 1
+
+Enter number: 32
+Enter number: 34
+Enter number: 54
+
+Average of the numbers is:  40.0
+
+             Program 2
+___INCOME TAX___
+• All taxpayers are charged a flat tax rate of 20%.
+• All taxpayers are allowed a $10,000 standard deduction.
+• For each dependent, a taxpayer is allowed an additional $3,000 deduction.
+• Gross income must be entered to the nearest penny.
+
+
+Enter your income to the nearest penny(without commas): $13243
+Enter the number of dependents in family: 3
+You don't have to pay income tax.
+Thanks for using our service.
+
+             Program 3
+
+Enter the number of students in class: 2
+
+Student ID: 21105036
+Student's Name: Sujal Aggarwal
+Student's Gender
+[Note: Use Gender values: ‘F’, ‘M’, ‘U’ (For Unknown).]: M
+Student's course: ECE
+Student's CGPA: 9.8
+
+Student ID: 21103939
+Student's Name: MOhit
+Student's Gender
+[Note: Use Gender values: ‘F’, ‘M’, ‘U’ (For Unknown).]: M
+Student's course: ECE
+Student's CGPA: 8.7
+
+
+Student List:-
+ [[21105036, 'Sujal Aggarwal', 'M', 'ECE', 9.8], [21103939, 'MOhit', 'M', 'ECE', 8.7]]
+
+             Program 4
+
+Student marks: 32
+Student marks: 43
+Student marks: 56
+Student marks: 78
+Student marks: 98
+
+Sorted List Of Student marks:-
+['32', '43', '56', '78', '98']
+
+             Program 5
+
+a.
+ ['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+b.
+ ['Red', 'Green', 'White', 'Purple', 'Yellow']
