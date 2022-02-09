a0fb3b37a2116c2214ba9bd012a8ca9bd6d5012e
SujalAggarwal <97459859+Sujal-Aggarwal@users.noreply.github.com>
2022-02-09 09:37:17 +0530

    Add files via upload

---
+++ Assignment 3/Output.py
@@ -0,0 +1,96 @@
+Question 1:
+
+Enter the string: Donald the duck said to other duck that it is not a duck it is just another duck
+The occurence(s) of:
+        "donald" is/are 1
+        "the" is/are 1
+        "duck" is/are 4
+        "said" is/are 1
+        "to" is/are 1
+        "other" is/are 1
+        "that" is/are 1
+        "it" is/are 2
+        "is" is/are 2
+        "not" is/are 1
+        "a" is/are 1
+        "just" is/are 1
+        "another" is/are 1
+
+ Question 2 is:
+
+Day - 31
+Month - 4
+Year - 2022
+The given month has only 30 days
+Please enter a valid date
+Day - 28
+Month - 4
+Year - 1980
+Next Date is: 29/4/1980
+
+ Question 3 is:
+
+Enter the list: [4,3,5,6,7,2,8,9]
+Output: [(4, 16), (3, 9), (5, 25), (6, 36), (7, 49), (2, 4), (8, 64), (9, 81)]
+
+ Question 4 is:
+
+Enter the grade point of the student: 9.0
+Your Grade is 'A' and Excellent Performance
+
+ Question 5 is:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+ Question 6 is:
+
+Enter the name of the student: Sujal Aggarwal
+Enter the SID of Sujal Aggarwal: 21105036
+
+You have entered 1 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: Maanik Jindal
+Enter the SID of Maanik Jindal: 21105050
+
+You have entered 2 value(s) till now
+Do you want to enter more data? n
+
+a. Student Details:
+The SID of Sujal Aggarwal is 21105036
+The SID of Maanik Jindal is 21105050
+
+b. Student Details (sorted with respect to names):
+The SID of Maanik Jindal is 21105050
+The SID of Sujal Aggarwal is 21105036
+
+c. Student Details (sorted with respect to SIDs):
+The SID of Sujal Aggarwal is 21105036
+The SID of Maanik Jindal is 21105050
+
+d. Enter the SID of the student: 3244
+The SID you entered isn't entered
+Please enter a valid SID to be searched
+List of valid SIDs: [21105036, 21105050]
+
+Enter the SID of the student: 21105036
+The name of student whose SID is 21105036 is Sujal Aggarwal
+
+ Question 7 is:
+
+Enter the no. of terms of the Fibonacci sequence: 10
+The Fibonacci sequence is as follows:
+0 1 1 2 3 5 8 13 21 34
+The average of the terms of Fibonacci sequence upto 10 terms is 8.80
+
+ Question 8 is:
+
+a. Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
