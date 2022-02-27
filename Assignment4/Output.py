daedc160043c1039a6524f337720bd11148ccfcb
SujalAggarwal <97459859+Sujal-Aggarwal@users.noreply.github.com>
2022-02-27 16:54:13 +0530

    Add files via upload

---
+++ Assignment4/Output.py
@@ -0,0 +1,90 @@
+-----------------------------------------------------------------
+Question 1
+
+The problem of tower of Hanoi with three disks.
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+------------------------------------------------------------------
+Question 2
+
+
+Enter the number of rows in Pascal's Triangle = 8
+
+Using recursions =
+
+              1
+            1   1
+          1   2   1
+        1   3   3   1
+      1   4   6   4   1
+    1   5   10   10   5   1
+  1   6   15   20   15   6   1
+1   7   21   35   35   21   7   1
+
+Using loops =
+
+              1
+            1   1
+          1   2   1
+        1   3   3   1
+      1   4   6   4   1
+    1   5   10   10   5   1
+  1   6   15   20   15   6   1
+1   7   21   35   35   21   7   1
+
+-------------------------------------------------------------------
+Question 3
+
+
+
+Enter the first integer = 4
+Enter the second integer = 5
+Remainder is =  4
+Quotient is =  0
+One value is zero
+{8}
+{8, 9}
+{8, 9, 5}
+{8, 9, 10, 5}
+{5, 6, 8, 9, 10}
+{5, 6, 8, 9, 10}
+Immutable set =  frozenset({5, 6, 8, 9, 10})
+Largest value in the set =  10
+Hash value =  10
+
+-------------------------------------------------------------------
+Question 4
+
+
+
+Object created
+The name of the student is Sujal Aggarwal and SID is 21105036.
+Object destroyed
+
+-------------------------------------------------------------------
+Question 5
+
+
+a. The updated salary of the employee Mohan is 70000
+b. Employee Vivek record deleted
+
+-------------------------------------------------------------------
+Question 6
+
+
+
+Enter the first word: Mic
+
+Enter a new meaningful word to test your friendship: hal
+The letters aren't exact, friendship is fake!!
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
